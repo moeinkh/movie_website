b6455c3b9abe5e5e6a5bbb75b4492d922e64f2f3
moein <moeinkhorami99@gmail.com>
2022-05-14 16:15:28 +0430

    add pagination to pages

--- product/views.py
+++ product/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Movie, Country, Poster
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 # Create your views here.
 def home(request):
@@ -8,9 +9,21 @@ def home(request):
     if search:
         movies = Movie.objects.filter(Q(persian_name__contains=search) | Q(name__contains=search))
     else:
-        movies = Movie.objects.all().order_by('-id')        
+        movies = Movie.objects.all().order_by('-id')    
+
+    paginator = Paginator(movies, 2)
+    page_number = request.GET.get('page')
+    try:
+        page_obj = paginator.get_page(page_number)  # returns the desired page object
+    except PageNotAnInteger:
+        # if page_number is not an integer then assign the first page
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        # if page is empty then return last page
+        page_obj = paginator.page(p.num_pages)
+    # end pagination config        
     context = {
-        'movies': movies,
+        'movies': page_obj,
         'category': Category.objects.all(),
         'countres': Country.objects.all(),
         'posters': Poster.objects.all(),
@@ -25,57 +38,148 @@ def detail(request, slug):
     }
     return render(request, 'product/detail.html', context)
 
-def category_cinematic(request, slug):
+def category_cinematic(request, slug):    
+    categories = Movie.objects.filter(category__slug=slug, type_movie=1).order_by('-id')
+
+    paginator = Paginator(categories, 2)
+    page_number = request.GET.get('page')
+    try:
+        page_obj = paginator.get_page(page_number)  # returns the desired page object
+    except PageNotAnInteger:
+        # if page_number is not an integer then assign the first page
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        # if page is empty then return last page
+        page_obj = paginator.page(p.num_pages)
+    # end pagination config 
     context={
-        'categores': Movie.objects.filter(category__slug=slug, type_movie=1).order_by('-id'),
+        'categores': page_obj,
         'category': Category.objects.all(),
         'countres': Country.objects.all(),
     }
     return render(request, 'product/category_cinematic.html', context)
 
-def category_serial(request, slug):
+def category_serial(request, slug):    
+    categories = Movie.objects.filter(category__slug=slug, type_movie=2).order_by('-id')
+
+    paginator = Paginator(categories, 2)
+    page_number = request.GET.get('page')
+    try:
+        page_obj = paginator.get_page(page_number)  # returns the desired page object
+    except PageNotAnInteger:
+        # if page_number is not an integer then assign the first page
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        # if page is empty then return last page
+        page_obj = paginator.page(p.num_pages)
+    # end pagination config 
     context={
-        'categores': Movie.objects.filter(category__slug=slug, type_movie=2).order_by('-id'),
+        'categores': page_obj,
         'category': Category.objects.all(),
         'countres': Country.objects.all(),
     }
     return render(request, 'product/category_serial.html', context)
 
 def country_cinematic(request, slug):
+    country = Movie.objects.filter(country__slug=slug, type_movie=1).order_by('-id')
+
+    paginator = Paginator(country, 2)
+    page_number = request.GET.get('page')
+    try:
+        page_obj = paginator.get_page(page_number)  # returns the desired page object
+    except PageNotAnInteger:
+        # if page_number is not an integer then assign the first page
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        # if page is empty then return last page
+        page_obj = paginator.page(p.num_pages)
+    # end pagination config 
     context = {
-        'country': Movie.objects.filter(country__slug=slug, type_movie=1).order_by('-id'),
+        'country': page_obj,
         'category': Category.objects.all(),
         'countres': Country.objects.all(),
     }    
     return render(request, 'product/country_cinematic.html', context)
 
 def country_serial(request, slug):
+    country = Movie.objects.filter(country__slug=slug, type_movie=2).order_by('-id')
+
+    paginator = Paginator(country, 2)
+    page_number = request.GET.get('page')
+    try:
+        page_obj = paginator.get_page(page_number)  # returns the desired page object
+    except PageNotAnInteger:
+        # if page_number is not an integer then assign the first page
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        # if page is empty then return last page
+        page_obj = paginator.page(p.num_pages)
+    # end pagination config 
     context = {
-        'country': Movie.objects.filter(country__slug=slug, type_movie=2).order_by('-id'),
+        'country': page_obj,
         'category': Category.objects.all(),
         'countres': Country.objects.all(),
     }    
     return render(request, 'product/country_serial.html', context)
 
 def film(request):
+    film = Movie.objects.filter(type_movie=1).order_by('-id')
+
+    paginator = Paginator(film, 2)
+    page_number = request.GET.get('page')
+    try:
+        page_obj = paginator.get_page(page_number)  # returns the desired page object
+    except PageNotAnInteger:
+        # if page_number is not an integer then assign the first page
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        # if page is empty then return last page
+        page_obj = paginator.page(p.num_pages)
+    # end pagination config 
     context = {
-        'film': Movie.objects.filter(type_movie=1).order_by('-id'),
+        'film': page_obj,
         'category': Category.objects.all(),
         'countres': Country.objects.all(),
     }    
     return render(request, 'product/film.html', context)
 
 def series(request):
+    series = Movie.objects.filter(type_movie=2).order_by('-id')
+
+    paginator = Paginator(series, 2)
+    page_number = request.GET.get('page')
+    try:
+        page_obj = paginator.get_page(page_number)  # returns the desired page object
+    except PageNotAnInteger:
+        # if page_number is not an integer then assign the first page
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        # if page is empty then return last page
+        page_obj = paginator.page(p.num_pages)
+    # end pagination config 
     context = {
-        'series': Movie.objects.filter(type_movie=2).order_by('-id'),
+        'series': page_obj,
         'category': Category.objects.all(),
         'countres': Country.objects.all(),
     }    
     return render(request, 'product/series.html', context)    
 
 def animation(request):
+    animation = Movie.objects.filter(category__title='انیمیشن').order_by('-id')
+
+    paginator = Paginator(animation, 2)
+    page_number = request.GET.get('page')
+    try:
+        page_obj = paginator.get_page(page_number)  # returns the desired page object
+    except PageNotAnInteger:
+        # if page_number is not an integer then assign the first page
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        # if page is empty then return last page
+        page_obj = paginator.page(p.num_pages)
+    # end pagination config 
     context = {
-        'animation': Movie.objects.filter(category__title='انیمیشن').order_by('-id'),
+        'animation': page_obj,
         'category': Category.objects.all(),
         'countres': Country.objects.all(),
     }    
